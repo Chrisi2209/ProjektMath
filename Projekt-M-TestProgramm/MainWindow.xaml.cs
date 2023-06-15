d5f2364cb5f0320fa786084342ff40216428e15e
S1m0n256 <simon.haas2005@gmail.com>
2023-06-15 10:41:28 +0200

    Create-Methods for classes: Number, Variable, OperationPart, Funktion, Fraction. Create new class: OperationPart

--- Projekt-M-TestProgramm/MainWindow.xaml.cs
+++ Projekt-M-TestProgramm/MainWindow.xaml.cs
@@ -28,10 +28,12 @@ namespace Projekt_M_TestProgramm
     }
 
 
+
     public class Expression
     {
         public DockPanel DockPanel { get; set; }
         public Expression BackPointer { get; set; }
+        public bool? Visible { get; set; }
         public int Index
         {
             get
@@ -40,7 +42,7 @@ namespace Projekt_M_TestProgramm
                 {
                     if (this == BackPointer[i]) return i;
                 }
-                return -1;
+                throw new Exception();
             }
         }
         public virtual int Length
@@ -55,13 +57,42 @@ namespace Projekt_M_TestProgramm
 
         static public Expression Create(string input, Expression backPointer = null)
         {
-
+            Expression expression;
+            while (true)
+            {
+                expression = NullExpression.Create(input);
+                if (expression != null) break;
+                expression = UnitExpression.Create(input);
+                if (expression != null) break;
+                expression = DoubleExpression.Create(input);
+                if (expression != null) break;
+                expression = RepetedExpression.Create(input);
+                if (expression != null) break;
+                throw new Exception();
+            }
+            expression.BackPointer = backPointer;
+            return expression;
         }
     }
 
 
     public class NullExpression : Expression
     {
+        static public NullExpression Create(string input)
+        {
+            NullExpression nullExpression;
+            while (true)
+            {
+                nullExpression = EmptyExpression.Create(input);
+                if (nullExpression != null) break;
+                nullExpression = Number.Create(input);
+                if (nullExpression != null) break;
+                nullExpression = Variable.Create(input);
+                if (nullExpression != null) break;
+                return null;
+            }
+            return nullExpression;
+        }
         public override int Length
         {
             get { return 0; }
@@ -94,6 +125,22 @@ namespace Projekt_M_TestProgramm
                 }
             }
         }
+
+        static public UnitExpression Create(string input)
+        {
+            UnitExpression unitExpression;
+            string nextInput;
+            while (true)
+            {
+                unitExpression = Funktion.Create(input, out nextInput);
+                if (unitExpression != null) break;
+                unitExpression = OperationPart.Create(input, out nextInput);
+                if (unitExpression != null) break;
+                return null;
+            }
+            unitExpression.Expression = Expression.Create(nextInput, unitExpression);
+            return unitExpression;
+        }
     }
 
     public class DoubleExpression : Expression
@@ -125,6 +172,20 @@ namespace Projekt_M_TestProgramm
                 }
             }
         }
+
+        static public DoubleExpression Create(string input)
+        {
+            DoubleExpression doubleExpression;
+            while (true)
+            {
+                doubleExpression = Fraction.Create(input);
+                if (doubleExpression != null) break;
+                doubleExpression = Power.Create(input);
+                if (doubleExpression != null) break;
+                return null;
+            }
+            return doubleExpression;
+        }
     }
 
     public class RepetedExpression : Expression
@@ -139,23 +200,62 @@ namespace Projekt_M_TestProgramm
             get { return Expressions[index]; }
             set { Expressions[index] = value; }
         }
+
+        static public RepetedExpression Create(string input)
+        {
+            RepetedExpression repetedExpression;
+            while (true)
+            {
+                repetedExpression = Sum.Create(input);
+                if (repetedExpression != null) break;
+                repetedExpression = Product.Create(input);
+                if (repetedExpression != null) break;
+                return null;
+            }
+            return repetedExpression;
+        }
     }
 
+    public class EmptyExpression : NullExpression
+    {
+        static public new EmptyExpression Create(string input)
+        {
+            if (input == "") return new EmptyExpression();
+            else return null;
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
 
     public class Number : NullExpression
     {
-        public long num;
+        private long num;
+
         public long Num
         {
             get { return num; }
             set { num = value; }
         }
 
-        static public Number Create(string input)
+        static public new Number Create(string input)
         {
             Number number = new Number();
+            if (input.Length == 0 || input[0] < '0' || '9' < input[0]) return null;
             if (!long.TryParse(input, out number.num)) number = null;
             return number;
+        }
+
+        static public bool IsDigit(char c)
+        {
+            return '0' <= c && c <= '9';
+        }
+
+        static public void GoBehindNumber(string input, ref int index)
+        {
+            while (index < input.Length && IsDigit(input[index])) index++;
         }
 
         public override string ToString()
@@ -168,10 +268,25 @@ namespace Projekt_M_TestProgramm
     {
         public string Name { get; set; }
 
-        static new public Variable Create(string input)
+        static public new Variable Create(string input)
         {
             Variable variable = new Variable();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] < 'a' || 'z' < input[i]) && (input[i] < 'A' || 'Z' < input[i])) return null;
+            }
+            variable.Name = input;
+            return variable;
+        }
 
+        static public bool IsLetter(char c)
+        {
+            return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
+        }
+
+        static public void GoBehindVariable(string input, ref int index)
+        {
+            while (index < input.Length && IsLetter(input[index])) index++;
         }
 
         public override string ToString()
@@ -180,15 +295,197 @@ namespace Projekt_M_TestProgramm
         }
     }
 
+    public class OperationPart : UnitExpression
+    {
+        public char Operation { get; set; }
+
+        static public OperationPart Create(string input, out string nextInput)
+        {
+            OperationPart operationPart = new OperationPart();
+            nextInput = null;
+            if (input.Length == 0) return null;
+            int index = 1;
+            switch (input[0])
+            {
+                case '+':
+                case '-':
+                    {
+                        while (index < input.Length)
+                        {
+                            switch (input[index++])
+                            {
+                                case '+':
+                                case '-': return null;
+                                case '*':
+                                case '/':
+                                case '^': break;
+                                case '(': Funktion.GoOutOfBracket(input, ref index); break;
+                                case char c when Number.IsDigit(c): Number.GoBehindNumber(input, ref index); break;
+                                case char c when Variable.IsLetter(c): Variable.GoBehindVariable(input, ref index); break;
+                            }
+                        }
+                        break;
+                    }
+                case '*':
+                    {
+                        while (index < input.Length)
+                        {
+                            switch (input[index++])
+                            {
+                                case '+':
+                                case '-':
+                                case '*':
+                                case '/': return null;
+                                case '^': break;
+                                case '(': Funktion.GoOutOfBracket(input, ref index); break;
+                                case char c when Number.IsDigit(c): Number.GoBehindNumber(input, ref index); break;
+                                case char c when Variable.IsLetter(c): Variable.GoBehindVariable(input, ref index); break;
+                            }
+                        }
+                        break;
+                    }
+                default: return null;
+            }
+            operationPart.Operation = input[0];
+            nextInput = input.Substring(1);
+            return operationPart;
+        }
+    }
+
     public class Funktion : UnitExpression
     {
         public string Name { get; set; }
-        public string BracketOpen { get; set; }
-        public string BracketClose { get; set; }
+        public bool? VisibleOpen { get; set; }
+        public char BracketOpen { get; set; }
+        public bool? VisibleClose { get; set; }
+        public char BracketClose { get; set; }
+
+        static public Funktion Create(string input, out string nextInput)
+        {
+            Funktion funktion = new Funktion();
+            nextInput = null;
+            if (input.Length < 2) return null;
+            switch (input[0])
+            {
+                case '(':
+                    {
+                        int index = 1;
+                        GoOutOfBracket(input, ref index);
+                        if (index < 0) throw new ArgumentOutOfRangeException();
+                        if (index < input.Length) return null;
+                        funktion.Name = "";
+                        break;
+                    }
+                case char c when Variable.IsLetter(c):
+                    {
+                        int index = 1;
+                        Variable.GoBehindVariable(input, ref index);
+                        if (index == input.Length || input[index++] != '(') return null;
+                        funktion.Name = input.Remove(index - 1);
+                        GoOutOfBracket(input, ref index);
+                        if (index < input.Length) return null;
+                        break;
+                    }
+                default: return null;
+            }
+            funktion.BracketOpen = '(';
+            funktion.BracketClose = ')';
+            nextInput = input.Substring(funktion.Name.Length + 1, input.Length - funktion.Name.Length - 2);
+            return funktion;
+        }
+
+        /// <summary>
+        /// searches for the correct bracketClose
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="index">this index starts inside of the brackets, returns -1 when not found</param>
+        /// <param name="bracketCounter"> '(' --> ++;     ')' --> --; </param>
+        static public void GoOutOfBracket(string input, ref int index, int bracketCounter = 1)
+        {
+            for (; index < input.Length && 0 < bracketCounter; index++)
+            {
+                switch (input[index])
+                {
+                    case '(': bracketCounter++; break;
+                    case ')': bracketCounter--; break;
+                }
+            }
+            if (0 < bracketCounter) index = -1;
+            return;
+        }
     }
 
     public class Fraction : DoubleExpression
     {
+        static public Fraction Create(string input, out string nextInputA, out string nextInputB)
+        {
+            Fraction fraction = new Fraction();
+            nextInputA = null;
+            nextInputB = null;
+            int index = 0;
+            while (true)
+            {
+                switch (input[index++])
+                {
+                    case '(':
+                        {
+                            Funktion.GoOutOfBracket(input, ref index);
+                            if (index < 0) throw new ArgumentOutOfRangeException();
+                            break;
+                        }
+                    case char c when Number.IsDigit(c):
+                        {
+                            Number.GoBehindNumber(input, ref index);
+                            break;
+                        }
+                    case char c when Variable.IsLetter(c):
+                        {
+                            Variable.GoBehindVariable(input, ref index);
+                            break;
+                        }
+                    default: return null;
+                }
+                if (index == input.Length) return null;
+                switch (input[index])
+                {
+                    case '/': break;
+                    case '^': continue;
+                    default: return null;
+                }
+                break;
+            }
+            nextInputA = input.Remove(index++);
+            if (index == input.Length) return null;
+            nextInputB = input.Substring(index);
+            while (true)
+            {
+                switch (input[index++])
+                {
+                    case '(':
+                        {
+                            Funktion.GoOutOfBracket(input, ref index);
+                            if (index < 0) throw new ArgumentOutOfRangeException();
+                            break;
+                        }
+                    case char c when Number.IsDigit(c):
+                        {
+                            Number.GoBehindNumber(input, ref index);
+                            break;
+                        }
+                    case char c when Variable.IsLetter(c):
+                        {
+                            Variable.GoBehindVariable(input, ref index);
+                            break;
+                        }
+                    default: return null;
+                }
+                if (index == input.Length) break;
+                if (input[index++] == '^') continue;
+                return null;
+            }
+            return fraction;
+        }
+
         public override string ToString()
         {
             return "(" + ExpressionA.ToString() + "/" + ExpressionB.ToString() + ")";
@@ -197,6 +494,11 @@ namespace Projekt_M_TestProgramm
 
     public class Power : DoubleExpression
     {
+        static public new Power Create(string input )
+        {
+            return null;
+        }
+
         public override string ToString()
         {
             return "(" + ExpressionA.ToString() + "^" + ExpressionB.ToString() + ")";
@@ -205,6 +507,11 @@ namespace Projekt_M_TestProgramm
 
     public class Sum : RepetedExpression
     {
+        static public new Sum Create(string input)
+        {
+            return null;
+        }
+
         public override string ToString()
         {
             if (Length == 0) return "(+)";
@@ -217,6 +524,11 @@ namespace Projekt_M_TestProgramm
 
     public class Product : RepetedExpression
     {
+        static public new Product Create(string input)
+        {
+            return null;
+        }
+
         public override string ToString()
         {
             if (Length == 0) return "(+)";
